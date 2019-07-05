caf45300aea6501363e933f8e73b30ca886dc604
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-05 23:29:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  reverse_sentence = sentence.split(" ").map do |word|
+  reverse_sentence = sentence.split(" ").collect do |word|
     word.reverse
   end
   reverse_sentence.join(" ")
